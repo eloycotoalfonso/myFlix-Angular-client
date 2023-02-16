a667a16695d0f8204995246ff20a1fa79299a8cf
eloycotoalfonso <eloycoal@gmail.com>
2023-02-16 11:30:38 +0100

    added movie details

--- src/app/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.scss']
 })
-export class MovieDetailsComponent {
+export class MovieDetailsComponent implements OnInit {
+  constructor(
+    @Inject (MAT_DIALOG_DATA)
+    public data: {
+      Title: string;
+      Description: string;
+    }
+  ){}
+
+  ngOnInit(): void {}
 
 }
