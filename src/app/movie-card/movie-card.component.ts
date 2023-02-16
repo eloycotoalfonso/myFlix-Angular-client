11acc7a52b0c5932a7d630211fc1842182af436e
eloycotoalfonso <eloycoal@gmail.com>
2023-02-16 11:30:21 +0100

    modified movie card

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -4,6 +4,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatDialog } from '@angular/material/dialog';
 import { GenreComponent } from '../genre/genre.component';
 import { DirectorComponent } from '../director/director.component';
+import { MovieDetailsComponent } from '../movie-details/movie-details.component';
 
 @Component({
   selector: 'app-movie-card',
@@ -21,13 +22,13 @@ export class MovieCardComponent {
 
   ngOnInit(): void {
     this.getMovies();
+    this.getFavoriteMovies();
   }
 
   getMovies(): void {
     this.fetchApiData.getAllMovies().subscribe((resp: any) => {
       this.movies = resp;
       console.log(this.movies);
-      // return this.movies;
     });
   }
 
@@ -48,5 +49,56 @@ export class MovieCardComponent {
       width: '400px',
     });
   }
+
+  openMovieDetails(movie: any): void {
+    const {Title, Description } = movie;
+    this.dialog.open(MovieDetailsComponent, {
+      data: {Title, Description},
+      panelClass: "details-dialog-background",
+      width: '400px'
+    });
+  }
+
+  getFavoriteMovies(): void {
+    this.fetchApiData.getFavoriteMovies().subscribe((resp: any) => {
+      this.favoriteMovies = resp[0].FavoriteMovies;
+      // console.log(this.favoriteMovies);
+    });
+  }
+
+  toggleFavorite(movie: any): void {
+    const id = movie._id;
+    // console.log(movie._id);
+    // console.log(id);
+    if (this.movieIsFavorite(id)){
+      console.log('this movie is favorite');
+      this.removeFromFavs(id);
+    } else {
+      console.log('this movie is not favorite');
+      // console.log(id);
+      this.addToFavs(id);
+    };
+  }
+
+  movieIsFavorite(id: string): boolean {
+    return this.favoriteMovies.includes(id);
+  }
+
+  addToFavs(id: string): void {
+    this.fetchApiData.addFavoriteMovie(id).subscribe((resp: any) => {
+      this.getFavoriteMovies();
+    });
+  }
+
+  removeFromFavs(id: string): void {
+    this.fetchApiData.removeFavoriteMovie(id).subscribe((resp: any) => {
+      this.getFavoriteMovies();
+    });
+  }
+
+  
+
+  
+
 
 }
